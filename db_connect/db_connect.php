ce86fdb817133a3e40605d4b96e652fa499a3264
Akram Zaabi <akramzaabi7@gmail.com>
2023-11-14 22:00:50 +0100

    db_connect

--- db_connect/db_connect.php
+++ db_connect/db_connect.php
@@ -1,1 +1,17 @@
-/** db_admin connect */
+<?php
+
+$pdo =new PDO('mysql:host=localhost;dbname=project_php','root','') ;
+
+$query_admin= $pdo->query('SELECT * FROM  admin ');
+$query_class=  $pdo->query('SELECT * FROM  class ');
+$query_document =  $pdo->query('SELECT * FROM  document ');
+
+
+ $admin = $query_admin->fetchAll();
+ $class = $query_class->fetchAll();
+ $document = $query_document->fetchAll();
+
+
+
+
+?>
