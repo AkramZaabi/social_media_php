297ced105d7c19ab1214bdbbbe41fdc875ec6b78
bouthaina <belgboutheina@gmail.com>
2023-12-01 19:22:01 +0100

    chat

--- message/index.php
+++ message/index.php
@@ -1,36 +1,30 @@
 <?php
     include '../DBconnect.php';
-    include 'C:\xampp\htdocs\project-php-sem1\db_connect/db_connect.php';
     $template="discussion";
     $titredepage="Chat";
     
     
 $idUtilisateur = 1;
 
-$sqlAmis = "SELECT id_user FROM amis where id_ami = '$idUtilisateur'";
-$resultAmis = $bdd->query($sqlAmis);
-$amis = [];
+$sqlAmis = 'SELECT id_user FROM amis where id_ami = :idUtilisateur';
+$resultAmis = $bdd->prepare($sqlAmis);
+$resultAmis->execute([
+    'idUtilisateur' => $idUtilisateur,
+]);
+$amis = $resultAmis->fetchAll(PDO::FETCH_ASSOC);
 
-
-while ($rowAmi = $resultAmis->fetch()) {
-    $idAmi = $rowAmi['id_user'];
-
-    $sqlMessages = "SELECT message.*, utilisateur.nom, utilisateur.prenom
-                    FROM message
-                    JOIN utilisateur ON (message.idEmetteur = utilisateur.idUtilisateur OR message.idRecepteur = utilisateur.idUtilisateur)
-                    WHERE (idEmetteur = $idUtilisateur AND idRecepteur = $idAmi) OR (idEmetteur = $idAmi AND idRecepteur = $idUtilisateur)
-                    ORDER BY message.date, message.heure DESC
-                    LIMIT 1"; // LIMIT 1 pour afficher seulement le dernier message
-
-    $resultMessages = $bdd->query($sqlMessages);
-
-    if ($rowMessage = $resultMessages->fetch()) {
-        $rowAmi['lastMessage'] = $rowMessage;
-    }
-
-    $amis[] = $rowAmi;
+foreach ($amis as $ami) {
+    $idAmi = $ami['id_user'];
+    $sqlInfoAmi = "SELECT nom, prenom FROM utilisateur WHERE idUtilisateur = :idAmi";
+    $resultInfoAmi = $bdd->prepare($sqlInfoAmi);
+    $resultInfoAmi->execute([
+        'idAmi' => $idAmi,
+    ]);
+    $infos = $resultInfoAmi->fetch(PDO::FETCH_ASSOC);
+    $ami['infoAmi'] = $infos;
+    $infosAmis[] = $ami;
 }
-var_dump($amis);
+
 $bdd = null;
 
 include './layout.phtml';
