8f5d64abbdefa3ee63fcb801877b928e4592f877
Mourad-Soltan <soltansoltanmourad@gmail.com>
2023-12-02 19:08:52 +0100

    mise a jour sign in and up

--- Login/sign_in.php
+++ Login/sign_in.php
@@ -1,7 +1,6 @@
 <?php
     if(isset($_POST['submit'])){
         extract($_POST);
-        //var_dump($_POST);
         $sql='SELECT * FROM utilisateur WHERE mail=:mail';
         $res=$pdo->prepare($sql);
         $res->execute([
