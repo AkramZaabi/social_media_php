64e0710e6e0fe8b61969f2b5fa102a9d8da480db
Mourad-Soltan <soltansoltanmourad@gmail.com>
2023-12-02 23:54:44 +0100

    message terminer

--- design/friend-accept.php
+++ design/friend-accept.php
@@ -1,5 +1,15 @@
 <?php
+require '../db_connect/db_connect.php';
 
-
+if(isset($_GET['id_ami']))
+{
+    $id_ami = $_GET['id_ami'];
+    $confirm=1;
+    
+    // Assuming "genreAmitie" is the column you want to update in the "todos" table
+    $sql = $pdo->prepare("UPDATE amis SET genreAmitie = ? WHERE id_ami = ?");
+    $sql->execute([$confirm,$id_ami]);
+}
+header('Location:./user.php');
 
 ?>
