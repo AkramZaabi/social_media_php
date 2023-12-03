cb17f3daa65de032cde625d3ec171f66eb79f76c
Akram Zaabi <akramzaabi7@gmail.com>
2023-12-03 11:33:50 +0100

    fixed-some-changes

--- message/index.php
+++ message/index.php
@@ -10,17 +10,34 @@
 $confirm = 1 ;    
     if( isset($_GET['id_ami']))
     {
-        echo ($_GET['id_ami']);
+        var_dump ($_GET['id_ami']);
+
+        
+        
+        $message_ami =  $pdo->prepare("SELECT *from message where (idEmetteur=? and idRecepteur=?) or (idEmetteur=? and idRecepteur=?) ORDER by idMsg");
+        $message_ami->execute([$_GET['id_ami'],$id,$id,$_GET['id_ami']]);
+        $results_message_ami= $message_ami->fetchAll(PDO::FETCH_ASSOC);
+        var_dump(($results_message_ami));
+
+        $user =  $pdo->prepare("SELECT * from utilisateur where idUtilisateur=?");
+        $user->execute([$id]);
+        $profile_user= $user->fetch(PDO::FETCH_ASSOC);
+
+        $profile_ami =  $pdo->prepare("SELECT * from utilisateur where idUtilisateur=?");
+        $profile_ami->execute([$id]);
+        $profile_ami= $profile_ami->fetch(PDO::FETCH_ASSOC);
     }
 $idUtilisateur = 1;
 $infosAmis = [];
-$friends_db =  $pdo->prepare("SELECT id_user from amis , utilisateur where id_ami =idUtilisateur and id_user =? and genreAmitie=?;");
+
+$friends_db =  $pdo->prepare("SELECT id_ami from amis where id_user=? and genreAmitie=?");
     $friends_db->execute([$id,$confirm]);
-    $amis= $friends_db->fetchAll(PDO::FETCH_ASSOC); 
+    $amis= $friends_db->fetchAll(PDO::FETCH_ASSOC);
+
 foreach ($amis as $ami) {
    
-    $idAmi = $ami['id_user'];
-    $sqlInfoAmi = "SELECT nom, prenom ,idUtilisateur FROM utilisateur WHERE idUtilisateur = :idAmi";
+    $idAmi = $ami['id_ami'];
+    $sqlInfoAmi = "SELECT nom, prenom ,idUtilisateur,photoProfile FROM utilisateur WHERE idUtilisateur = :idAmi";
     $resultInfoAmi = $pdo->prepare($sqlInfoAmi);
     $resultInfoAmi->execute([
         'idAmi' => $idAmi,
@@ -30,8 +47,6 @@ foreach ($amis as $ami) {
     //var_dump($ami['infoAmi']['nom']);
     array_push($infosAmis,$ami['infoAmi']);
 }
-
-var_dump($infosAmis); 
 
 $pdo = null;
 
