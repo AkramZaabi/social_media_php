3d09e353362829bda961333dec62b14efffd0020
Akram Zaabi <akramzaabi7@gmail.com>
2023-12-09 16:32:58 +0100

    fixzd-friend-profile

--- profile/profile.php
+++ profile/profile.php
@@ -6,12 +6,25 @@
     }
     include '../db_connect/db_connect.php';
     
-    $req='SELECT * FROM utilisateur where idUtilisateur=?';
-    $res=$pdo->prepare($req);
-    $res->execute([
-        $_SESSION['idUtilisateur'],
-    ]);
-    $user=$res->fetch();
+    if(isset($_GET['id_ami']))
+    {
+        $id_ami = $_GET['id_ami'];    
+        $req='SELECT * FROM utilisateur where idUtilisateur=?';
+        $res=$pdo->prepare($req);
+        $res->execute([
+            $id_ami,
+        ]);
+        $user = $res->fetch();
+    }
+    else{
+
+        $req='SELECT * FROM utilisateur where idUtilisateur=?';
+        $res=$pdo->prepare($req);
+        $res->execute([
+            $_SESSION['idUtilisateur'],
+        ]);
+        $user=$res->fetch();
+    }
     $template="profile";
     $titredepage="Profile";
     include 'layout.phtml';
