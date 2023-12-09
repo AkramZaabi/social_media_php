a57170fd0574d8ca22632e86b5f8860f92eb0fae
Akram Zaabi <akramzaabi7@gmail.com>
2023-12-09 18:14:55 +0100

    post-fix

--- design/post.php
+++ design/post.php
@@ -4,7 +4,9 @@ session_start();
 $id =$_SESSION['idUtilisateur'];
 if(isset($_POST['submit'])){
     $pub = $_POST['pub'];
-$imageName = $_FILES['image']['name'];
+    if($_FILES['image']['name']!='')
+    {
+        $imageName = $_FILES['image']['name'];
 $imageTypeExtension = pathinfo($imageName, PATHINFO_EXTENSION);
 $newImageName = md5($imageName) . '.' . $imageTypeExtension;
 $newImagePath = 'assets/' . $newImageName;
@@ -15,9 +17,16 @@ if ($_FILES['image']['error'] !== UPLOAD_ERR_OK) {
     echo ('Image upload failed with error code ' . $_FILES['image']['error']);
 } else {
     // Process successful image upload
+    $newImagePath="";
+}
+    }
+else{
+    $newImagePath="";
 }
 
-$videoName = $_FILES['video']['name'];
+if($_FILES['video']['name']!='')
+{
+    $videoName = $_FILES['video']['name'];
 $videoTypeExtension = pathinfo($videoName, PATHINFO_EXTENSION);
 $newVideoName = md5($videoName) . '.' . $videoTypeExtension;
 $newVideoPath = 'assets/' . $newVideoName;
@@ -27,8 +36,14 @@ move_uploaded_file($_FILES['video']['tmp_name'], $newVideoPath);
 if ($_FILES['video']['error'] !== UPLOAD_ERR_OK) {
     echo ('Video upload failed with error code ' . $_FILES['video']['error']);
 } else {
+    $newVideoPath = "" ; 
     // Process successful video upload
 }
+}
+else{
+    $newVideoPath = "" ;
+}
+
     $currentDate = date('Y-m-d');
     $heure = date("h:i:sa");
     $sql='insert into publication(idUtilisateur,contenuPub,video,image,datePub,heurePub) values(?,?,?,?,?,?);';
